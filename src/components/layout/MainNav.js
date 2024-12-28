077e1ea7a6f04885874c9345dd969aa8a5e91e64
shvabriq <s.shibakova@mail.ru>
2024-12-29 04:17:47 +0500

    update nav

--- src/components/layout/MainNav.js
+++ src/components/layout/MainNav.js
@@ -4,35 +4,32 @@ import { Link } from "react-router-dom";
 import classes from "./MainNav.module.css";
 import logo from "../../images/logo.png";
 import header from "../../images/header.jpg";
+import { Navbar, Nav } from 'react-bootstrap';
 
 function MainNav() {
     return (
         <div>
-            <header className={classes.header}>
-                <div className={classes.logo} >
+            <Navbar collapseOnSelect expand="lg" className={classes.header}>
+                <Navbar.Brand className={classes.logo}>
                     <div className="animate__animated animate__rotateIn">
                         <img className={classes.img_logo} src={logo} alt="logo" />
                         <span>Pets farm</span>
                     </div>
-                </div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/pets">Pets</Link>
-                        </li>
-                        <li>
-                            <Link to="/services">Services</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/pets">Pets</Nav.Link>
+                        <Nav.Link as={Link} to="/services">Services</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
             <div>
                 <img className={classes.img_header} src={header} alt="header" />
             </div>
         </div>
     );
 }
-export default MainNav; 
+
+export default MainNav;
